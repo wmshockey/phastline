a22760b88ae10c4abdc0801d4e6eba15a09622b0
Warren Shockey <wmshockey@gmail.com>
2020-12-27 18:25:09 -0700

    fixed config problem with development.rb and production.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,19 +58,21 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  # Config for Devise
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = {host: 'phastware.com'}
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      user_name:      ENV["GMAIL_USERNAME"],
+      password:       ENV["GMAIL_PASSWORD"],
+      domain:         'smtp.google.com',
+      address:       'smtp.gmail.com',
+      port:          '587',
+      authentication: :plain,
+      enable_starttls_auto: true
+    }
+    
 end
 
-# Config for Devise
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.perform_deliveries = true
-config.action_mailer.default_url_options = {host: 'phastware.com'}
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    user_name:      ENV["GMAIL_USERNAME"],
-    password:       ENV["GMAIL_PASSWORD"],
-    domain:         'smtp.google.com',
-    address:       'smtp.gmail.com',
-    port:          '587',
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,19 +91,21 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # Config for Devise
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = {host: 'phastware.com'}
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      user_name:      ENV["GMAIL_USERNAME"],
+      password:       ENV["GMAIL_PASSWORD"],
+      domain:         'smtp.google.com',
+      address:       'smtp.gmail.com',
+      port:          '587',
+      authentication: :plain,
+      enable_starttls_auto: true
+    }
+    
 end
 
-# Config for Devise
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.perform_deliveries = true
-config.action_mailer.default_url_options = {host: 'phastware.com'}
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    user_name:      ENV["GMAIL_USERNAME"],
-    password:       ENV["GMAIL_PASSWORD"],
-    domain:         'smtp.google.com',
-    address:       'smtp.gmail.com',
-    port:          '587',
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
